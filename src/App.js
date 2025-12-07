adf0120e0dea57536a26760ba455dc853c7f3eb9
Corman11 <156091016+Corman11@users.noreply.github.com>
2025-12-07 23:45:56 +0100

    Main.js nie importuje sie

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import './cssFiles/style.css'
+import SpanishFlag from './images/Spanish_Flag.png'
+import TaskFrase from './pages/TaskFrase';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+
+function FlagImage() {
+  return(
+
+    <div className='b'>
+      <img src={SpanishFlag} alt="flaga" style={{ width: '400px', }}/>
+    </div>
+  )
+
+
+}
+
+
+function Layout() {
+  return (
+    <>
+      <div className="top-line"></div>
+      
+      
+      {/* STRONY BĘDĄ WSTAWIANE TUTAJ */}
+      <Outlet />
+
+      <div className="bottom-line"></div>
+    </>
+  );
+}
+
+
+//Odpowiednik for-each
+function MainPage() {
+  const cars = ['Leccion 1 -> Praca z tekstem', 'Leccion 2 -> Praca ze zdaniami', 'Leccion 3 -> przydatne argumenty'];
+  return (
+    <>
+    <div className="top-line"></div>
+    <FlagImage />
+     <div className="center">
+          <h1 className='Map'>Apka Profe Beata</h1>
+          
+
+  <ul className="text">
+    {cars.map(car => <li>{car}</li>)}
+    
+  </ul>
+  <Link to="/TaskFrase">Zadanie z Boxami prototyp</Link>
+  </div>
+    </>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    
+
+    
+    
+    
+     <BrowserRouter>
+      <Routes>
+        {/* Layout wspólny */}
+        <Route path="/" element={<Layout />}>
+          
+          {/* Strona główna */}
+          <Route index element={<MainPage />} />
+
+          {/* Zadanie */}
+          <Route path="TaskFrase" element={<TaskFrase />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+      
+    
+    
   );
 }
 
